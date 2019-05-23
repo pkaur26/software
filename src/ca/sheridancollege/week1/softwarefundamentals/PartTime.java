d476daab6358b91c843f95314bcc570ca6dc8362
pkaur26 <kaur6643@sheridancollege.ca>
2019-05-23 13:00:08 -0400

    software

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/PartTime.java
@@ -0,0 +1,34 @@
+/*
+ * Parneet Kaur
+ * Student ID = 991544633
+ * SYST10199-WEB PROGRAMMING
+ */
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ *
+ * @author parne
+ */
+public class PartTime {
+    
+    private int numClass;
+
+    /**
+     * Get the value of numClass
+     *
+     * @return the value of numClass
+     */
+    public int getNumClass() {
+        return numClass;
+    }
+
+    /**
+     * Set the value of numClass
+     *
+     * @param numClass new value of numClass
+     */
+    public void setNumClass(int numClass) {
+        this.numClass = numClass;
+    }
+
+}
